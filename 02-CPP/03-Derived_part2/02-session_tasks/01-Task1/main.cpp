224feef6246a0e55510c8c078d448ebcbac777ef
UsefElbedwehy <elbedwehyy@gmail.com>
2024-08-18 08:53:20 +0300

    1st task: max number in array

---
+++ 02-CPP/03-Derived_part2/02-session_tasks/01-Task1/main.cpp
@@ -0,0 +1,33 @@
+//author: Usef Elbedwehy
+//date: Sun 18 Aug 2024
+//@session: 03
+//@topic: derived part 2
+//@brief: (first task) -> create a function to ﬁnd the maximum number of array
+
+#include <iostream>
+
+int max_number(int* arr, int arr_size){
+
+    int local_max_number = arr[0]; // assume max is the first element
+
+    if(arr_size == 0){
+    std::cout << "Array is empty." << std::endl;
+    return -1 ; // return -1 as status for empty array
+    }
+    for(int i=1;i<arr_size;i++){
+        if(local_max_number < arr[i]){
+            local_max_number = arr[i]; // update max
+        }
+    }
+    return local_max_number;
+}
+
+int main(){
+
+    int arr[8] = {1,23,4,5,6,767,543,768};
+    int size = sizeof(arr)/sizeof(arr[0]); // calc size of array
+    std::cout << "The max number in the array is: " << max_number(arr,size) << std::endl;
+
+
+    return 0;
+}
