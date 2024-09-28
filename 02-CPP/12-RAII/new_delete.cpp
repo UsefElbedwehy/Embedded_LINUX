0390773b0e7498e939a40eec0dcc5ec34fe02802
UsefElbedwehy <elbedwehyy@gmail.com>
2024-09-28 11:32:35 +0300

    manage and check memory leakage using valgrind

---
+++ 02-CPP/12-RAII/new_delete.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+
+// steps to check the memory leakage:
+// 1- compile the program: g++ -g new_delete.cpp && ./a.out 
+// run the program using valgring : valgrind --leak-check=full ./a.out 
+
+int main(int argc, char const *argv[])
+{
+    int *p = new int{5};
+    *p = 6;
+    std::cout << "after creation: *p: "<< *p << std::endl;
+    delete p;
+    std::cout << "after deletion: *p: "<< *p << std::endl;
+    *p = 6;
+    std::cout << "after deletion and modification: *p: "<< *p << std::endl;
+
+
+    int *pp = new int[3]{1,2,3};
+    std::for_each(&pp[0],&pp[3],[](int i){std::cout << i << " ";});
+    std::cout << std::endl;
+    delete [] pp;
+    std::for_each(&pp[0],&pp[3],[](int i){std::cout << i << " ";});
+    std::cout << std::endl;
+
+
+
+    return 0;
+}
