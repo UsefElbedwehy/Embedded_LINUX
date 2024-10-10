86547107350a543bbec1a76d56c11a5d00a33cf9
UsefElbedwehy <elbedwehyy@gmail.com>
2024-10-10 21:53:13 +0300

    main app for server(PC)/client(mobile) socket

---
+++ projects/cpp_project/mobile_control_pc_socket_prog/main.cpp
@@ -0,0 +1,20 @@
+
+#include "server.hpp"
+#include <cstring>
+#include <string>
+#include "commands.hpp"
+#include "terminal.hpp"
+
+int main(int argc, char const *argv[])
+{
+    const int portNumber = 8080;
+    Commands cmd;
+    Terminal ter;
+    std::string command;
+
+    Server server(portNumber);
+
+    server.startHttp();
+
+    return 0;
+}
