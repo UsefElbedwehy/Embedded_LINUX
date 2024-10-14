08886cc386ca7f222ab725c5805b465ba92f1e35
UsefElbedwehy <elbedwehyy@gmail.com>
2024-10-14 15:27:42 +0300

    update terminal methods

--- projects/cpp_project/mobile_control_pc_socket_prog/terminal.cpp
+++ projects/cpp_project/mobile_control_pc_socket_prog/terminal.cpp
@@ -1,12 +1,12 @@
 #include "terminal.hpp"
 #include <iostream>
-#define WELCOME_THEME       WHITE
+#include <string>
 
 void Terminal::showTitle(const std::string &title)
 {
-    std::cout << WELCOME_THEME <<"-----------------------------" << std::endl;
-    std::cout <<                 "---------- "<< title <<" -----------" << std::endl;
-    std::cout <<                 "-----------------------------" << RESET << std::endl;
+    std::cout << BLUE << std::string(29,'-') << std::endl;
+    std::cout << "---------- "<< title <<" -----------" << std::endl;
+    std::cout << std::string(29,'-') << RESET << std::endl;
 }
 
 void Terminal::print(const std::string &msg)
@@ -21,5 +21,35 @@ void Terminal::printInColor(const std::string &msg, const std::string &color)
 void Terminal::printInColorSameline(const std::string &msg1,const std::string &msg2, const std::string &color)
 {
     std::cout << color << msg1 << RESET << msg2<< std::endl;
+}
+// ----------------------------------------------------------------------------------
+void Terminal::printErrorMsg(const std::string &msg)
+{
+    std::cout << RED << msg << RESET ;
+}
+
+void Terminal::printConnectMsg(const std::string &msg)
+{
+    std::cout << GREEN << msg << RESET ;
+}
+
+void Terminal::printWarningMsg(const std::string &msg)
+{
+    std::cout << YELLOW << msg << RESET ;
+}
+
+void Terminal::printRecvMsg(const std::string &msg)
+{
+    std::cout << MAGENTA << msg << RESET ;
+}
+
+void Terminal::printSentMsg(const std::string &msg)
+{
+    std::cout << BLUE << msg << RESET ;
+}
+
+void Terminal::printMsg(const std::string &msg)
+{
+    std::cout << WHITE << msg << RESET ;
 }
 
--- projects/cpp_project/mobile_control_pc_socket_prog/terminal.hpp
+++ projects/cpp_project/mobile_control_pc_socket_prog/terminal.hpp
@@ -7,18 +7,27 @@ class Terminal
 {
 protected:
     // ANSI escape codes for colors
-    const std::string WHITE     = "\033[37m";
-    const std::string CYAN      = "\033[36m";
-    const std::string RED       = "\033[31m";
-    const std::string GREEN     = "\033[32m";
-    const std::string YELLOW    = "\033[33m";
-    const std::string RESET     = "\033[0m";
+    const std::string BLACK = "\033[30m";
+    const std::string RED = "\033[31m";
+    const std::string GREEN = "\033[32m";
+    const std::string YELLOW = "\033[33m";
+    const std::string BLUE = "\033[34m";
+    const std::string MAGENTA = "\033[35m";
+    const std::string CYAN = "\033[36m";
+    const std::string WHITE = "\033[37m";
+    const std::string RESET = "\033[0m";
 public:
     void showTitle(const std::string &title);
     void print(const std::string &msg);
     void printInColor(const std::string &msg,const std::string& color);
     void printInColorSameline(const std::string &msg1,const std::string &msg2, const std::string &color);
 
+    void printErrorMsg(const std::string &msg);
+    void printConnectMsg(const std::string &msg);
+    void printWarningMsg(const std::string &msg);
+    void printRecvMsg(const std::string &msg);
+    void printSentMsg(const std::string &msg);
+    void printMsg(const std::string &msg);
 };
 
 
